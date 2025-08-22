7b2f27b05bcc0e166499210bf74f0de81a32be89
Admishra55 <achrutet@gmail.com>
2025-08-22 17:25:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,21 @@
-const CACHE = 'an-study-v3';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/logo_192x192.png',
-  '/logo_512x512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
+self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.open("anstudy-cache").then(cache=>{
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./logo_192x192.png",
+        "./logo_512x512.png"
+      ]);
+    })
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(resp=>{
+      return resp || fetch(e.request);
+    })
   );
 });
